e4df420d570ad4a30c57228bfacd6ac517167cbd
chchen <chchen@149-159-219-239.dhcp-bl.indiana.edu>
2015-09-08 22:55:28 -0400

    From https://github.com/iu-parfunc/adaptive-data

---
+++ Entry.hs
@@ -0,0 +1,3 @@
+module EntryVal
+
+data EntryVal t = Val t | Copied t
---
+++ OldBag.hs
@@ -0,0 +1,34 @@
+{-# LANGUAGE BangPatterns #-}
+
+-- The old way of doing things: atomicModifyIORef
+
+module OldBag
+       (
+         PureBag
+       , newBag
+       , add
+       , remove
+       )
+       where
+
+import Data.Atomics
+import Data.IORef
+import EntryVal
+
+type PureBag a = IORef [a]
+
+{-# INLINABLE newBag #-}
+newBag :: IO (PureBag a)
+newBag = newIORef []
+
+{-# INLINABLE add #-}
+add :: PureBag a -> a -> IO ()
+add bag !x = atomicModifyIORef' bag (\l -> (x:l,()))
+
+{-# INLINABLE remove #-}
+remove :: PureBag a -> IO (Maybe a)
+remove bag = atomicModifyIORef' bag fn
+ where
+  fn [] = ([],Nothing)
+  fn (h:t) = (t,Just h)
+
